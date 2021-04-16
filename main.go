938cd7f84f354edc8f255e68ca7c8ff002cd1477
meutraa <paul@lost.host>
2021-04-16 15:40:02 +0100

    Add more stats, stdev, chart selection, mines, miss flashes

--- main.go
+++ main.go
@@ -23,9 +23,11 @@ import (
 )
 
 const (
-	globalOffset  = 0.033
+	globalOffset  = 0.00
+	flashLength   = 60
 	speed         = 12
 	bottomPadding = 8
+	mineSym       = "╳"
 	framePeriod   = 4166666 * time.Nanosecond
 )
 
@@ -101,7 +103,7 @@ func fill(x, y int64, c string) string {
 }
 
 func fillColor(x, y int64, color, c string) string {
-	return fmt.Sprintf("\033[%d;%dH%v%v", y, x, color, c)
+	return fmt.Sprintf("\033[%d;%dH%v%v\033[0m", y, x, color, c)
 }
 
 func replace(x, y, p, q int64, c string) string {
@@ -112,8 +114,8 @@ func bar(message string) string {
 	return fmt.Sprintf("\033[K\033[0;0H%v", message)
 }
 
-func side(line int, message string) string {
-	return fmt.Sprintf("\033[%v;2H%v", line, message)
+func side(line int, sc int64, message string) string {
+	return fmt.Sprintf("\033[%v;%vH%v", line, sc, message)
 }
 
 func main() {
@@ -130,9 +132,17 @@ type Note struct {
 	bpm         float64
 	length      float64
 	hit         bool
+	isMine      bool
 	miss        bool
-	hitTime     int64
-	ms          int64
+	// A list of co-ords that need clearing when remaining tick == 1
+	missFlash          *map[string]Point
+	missFlashRemaining int
+	hitTime            int64
+	ms                 int64
+}
+
+type Point struct {
+	c, r int64
 }
 
 func isRowInField(rc int64, row int64, hit bool) bool {
@@ -141,27 +151,56 @@ func isRowInField(rc int64, row int64, hit bool) bool {
 
 func (note *Note) step(now time.Time, currentDuration time.Duration, cis *[4]int64, rc int64) (str string, miss int) {
 	// clear all existing renders
+	col := cis[note.col]
 	if isRowInField(rc, note.row, false) {
-		str += fill(cis[note.col], note.row, " ")
+		str += fill(col, note.row, " ")
 	}
+	if note.missFlashRemaining > 1 {
+		note.missFlashRemaining--
+	} else if note.missFlashRemaining == 1 {
+		note.missFlashRemaining--
+		// clear flash
+		for _, r := range *note.missFlash {
+			str += fill(r.c, r.r, " ")
+		}
+	} // else 0 and does not exist anymore
 
 	// Calculate the new row based on time
-	// 43 - 41 = 2 away = rc - 4 - 2 away
-	distance := (note.ms - currentDuration.Milliseconds()) / speed
-	note.row = (rc - bottomPadding) - distance
+	nr := (rc - bottomPadding)
+	distance := int64(math.Round(float64((note.ms - currentDuration.Milliseconds())) / float64(speed)))
+	note.row = nr - distance
 
 	// Is this row within the playing field?
-	if !note.miss && note.row > rc && !note.hit {
+	if !note.miss && note.row > rc && !note.hit && !note.isMine {
 		miss = 1
 		note.miss = true
+		// flash the miss
+		note.missFlashRemaining = flashLength
+		cen := rc >> 1
+		note.missFlash = &map[string]Point{
+			"╭": {col - 1, cen - 1},
+			"╮": {col + 1, cen - 1},
+			"╰": {col - 1, cen},
+			"╯": {col + 1, cen},
+		}
+		for c, r := range *note.missFlash {
+			str += fillColor(r.c, r.r, "\033[1;31m", c)
+		}
 	} else if isRowInField(rc, note.row, note.hit) {
-		str += fillColor(cis[note.col], note.row, note.color, syms[note.col])
+		if note.isMine {
+
+			str += fillColor(col, note.row, "\033[1;31m", mineSym)
+		} else {
+			str += fillColor(col, note.row, note.color, syms[note.col])
+		}
 	}
 	return
 }
 
 type Chart struct {
-	notes []*Note
+	notes     []*Note
+	noteCount int64
+	mineCount int64
 }
 
 type BPM struct {
@@ -184,7 +223,13 @@ func getSecondsPerNote(rates []BPM, currentBeat float64, bpn float64) (float64, 
 	return sel, bpn * secondsPerBeat
 }
 
-func parse(file string) (*Chart, error) {
+type Difficulty struct {
+	name    string
+	msd     string
+	section string
+}
+
+func parse(ch <-chan keyboard.KeyEvent, file string) (*Chart, error) {
 	data, err := ioutil.ReadFile(file)
 	if nil != err {
 		return nil, err
@@ -193,15 +238,31 @@ func parse(file string) (*Chart, error) {
 	sections := strings.Split(string(data), "#NOTES:")
 	meta := sections[0]
 	notesSection := ""
+	difficulties := []Difficulty{}
 	// difs := []string{"Challenge", "Hard", "Beginner"}
 	for _, section := range sections[1:] {
-		if !strings.Contains(section, "dance-single") {
+		// Todo difficulty selection
+		lines := strings.SplitN(section, "\n", 7)
+		if !strings.Contains(lines[1], "dance-single") {
 			continue
 		}
-		// Todo difficulty selection
-		notesSection = section
-		break
+		difficulties = append(difficulties, Difficulty{
+			name:    strings.TrimSpace(lines[3]),
+			msd:     strings.TrimSpace(lines[4]),
+			section: lines[6],
+		})
 	}
+
+	for i, dif := range difficulties {
+		fmt.Printf("%v\t%3v\t%v\n", i, dif.msd, dif.name)
+	}
+	key := <-ch
+	index, err := strconv.ParseInt(string(key.Rune), 10, 64)
+	if nil != err || index > int64(len(difficulties)-1) {
+		return nil, err
+	}
+
+	notesSection = difficulties[index].section
 
 	offset := 0.0
 	bpms := []BPM{}
@@ -242,6 +303,8 @@ func parse(file string) (*Chart, error) {
 	currentBeat := float64(0.0)
 
 	notes := []*Note{}
+	mineCount := 0
+	noteCount := 0
 
 	blocks := strings.Split(notesSection, "\n,")
 	for _, block := range blocks {
@@ -268,28 +331,34 @@ func parse(file string) (*Chart, error) {
 			// log.Printf("%v/%v = 1/%v", i+1, len(lines), denom)
 			bpm, secondsPerNote := getSecondsPerNote(bpms, currentBeat, beatsPerNote)
 
-			createNote := func(col int) *Note {
+			createNote := func(col int, c string) *Note {
+				if c == "M" {
+					mineCount++
+				} else {
+					noteCount++
+				}
 				return &Note{
 					col:         col,
 					color:       getColor(denom),
 					currentBeat: currentBeat,
 					length:      secondsPerNote,
 					bpm:         bpm,
+					isMine:      c == "M",
 					ms:          int64(t * 1000),
 				}
 			}
 
 			if mapToNote(chs[0]) {
-				notes = append(notes, createNote(0))
+				notes = append(notes, createNote(0, string(chs[0])))
 			}
 			if mapToNote(chs[1]) {
-				notes = append(notes, createNote(1))
+				notes = append(notes, createNote(1, string(chs[1])))
 			}
 			if mapToNote(chs[2]) {
-				notes = append(notes, createNote(2))
+				notes = append(notes, createNote(2, string(chs[2])))
 			}
 			if mapToNote(chs[3]) {
-				notes = append(notes, createNote(3))
+				notes = append(notes, createNote(3, string(chs[3])))
 			}
 
 			t += secondsPerNote
@@ -298,7 +367,9 @@ func parse(file string) (*Chart, error) {
 	}
 
 	return &Chart{
-		notes: notes,
+		notes:     notes,
+		noteCount: int64(noteCount),
+		mineCount: int64(mineCount),
 	}, nil
 }
 
@@ -314,7 +385,7 @@ func parse(file string) (*Chart, error) {
 
 func mapToNote(ch byte) bool {
 	t := string(ch)
-	return t == "1" || t == "2" || t == "4"
+	return t == "1" || t == "2" || t == "M"
 }
 
 // Returns an index of the judgement array
@@ -345,13 +416,6 @@ func run() error {
 		}
 	}()
 
-	// Clear the screen and hide the cursor
-	fmt.Printf("\033[?1049h\033[?25l\033[H\033[J")
-	defer func() {
-		// Restore the terminal state
-		fmt.Printf("\033[?1049l\033[?25h")
-	}()
-
 	var mp3File, ogg, chartFile string
 
 	if err := filepath.Walk(*song, func(path string, info os.FileInfo, err error) error {
@@ -372,10 +436,12 @@ func run() error {
 	}
 
 	mc := int64(ws.Col) >> 1
+	rc := int64(ws.Row)
 	space := int64(6)
 	cis := &([4]int64{mc - space*3, mc - space, mc + space, mc + space*3})
+	sideCol := mc - space*9
 
-	chart, err := parse(chartFile)
+	chart, err := parse(keyChannel, chartFile)
 	if nil != err {
 		return err
 	}
@@ -384,6 +450,7 @@ func run() error {
 	if ogg != "" {
 		audioFile = ogg
 	}
+	log.Printf("Opening %v (%v)\n", audioFile, chartFile)
 	f, err := os.Open(audioFile)
 	if err != nil {
 		return err
@@ -395,7 +462,6 @@ func run() error {
 	} else {
 		streamer, format, err = mp3.Decode(f)
 	}
-	vorbis.Decode(f)
 	if err != nil {
 		return err
 	}
@@ -404,19 +470,22 @@ func run() error {
 	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/60))
 	speaker.Play(streamer)
 
+	// Clear the screen and hide the cursor
+	fmt.Printf("\033[?1049h\033[?25l\033[H\033[J")
+	defer func() {
+		// Restore the terminal state
+		fmt.Printf("\033[?1049l\033[?25h")
+	}()
+
 	startTime := time.Now()
 	score := 0.0
-	totalNotes := len(chart.notes)
-	counts := [8]int{
-		0, // 6 Exact
-		0, // 11 Ridic
-		0, // 22 Marv
-		0, // 45 Perf
-		0, // 90 Great
-		0, // 135 Good
-		0, // 180 Boo
-		0, // Miss
-	}
+	counts := [8]int{0, 0, 0, 0, 0, 0, 0, 0}
+
+	// stdev
+	sumOfDistance := 0.0
+	mean := 0.0
+	totalHits := 0.0
+	stdev := 0.0
 
 	for {
 		now := time.Now()
@@ -436,16 +505,21 @@ func run() error {
 			}
 			var closestNote *Note
 			distance := 10000000.0
+			dirDistance := 1000000.0
 			for _, note := range chart.notes {
 				if (note.hit) ||
+					(note.isMine) ||
 					(note.col != 0 && string(key.Rune) == keys[0]) ||
 					(note.col != 1 && string(key.Rune) == keys[1]) ||
 					(note.col != 2 && string(key.Rune) == keys[2]) ||
 					(note.col != 3 && string(key.Rune) == keys[3]) {
 					continue
 				}
-				d := math.Abs(float64(note.ms) - float64(currentDuration.Milliseconds()))
+				dd := float64(note.ms - currentDuration.Milliseconds())
+				d := math.Abs(dd)
+				// log.Printf("hiiiiiit %v %v = %v", note.ms, currentDuration.Milliseconds(), dd)
 				if d < distance {
+					dirDistance = dd
 					distance = d
 					closestNote = note
 				} else if nil != closestNote {
@@ -455,21 +529,38 @@ func run() error {
 			}
 			if nil != closestNote && distance < judgements[len(judgements)-1] {
 				score += distance
+				totalHits += 1
+				sumOfDistance += dirDistance
 				judgement := judge(distance)
 				counts[judgement] = counts[judgement] + 1
 				closestNote.hitTime = currentDuration.Milliseconds()
 				closestNote.hit = true
+				if totalHits > 2 {
+					stdev = 0.0
+					mean = sumOfDistance / totalHits
+					for _, n := range chart.notes {
+						if !n.hit {
+							continue
+						}
+						diff := float64(n.ms - n.hitTime)
+						xi := diff - mean
+						xi2 := xi * xi
+						stdev += xi2
+					}
+					stdev /= (totalHits - 1)
+					stdev = math.Sqrt(stdev)
+				}
 			}
 		}
 
 		// Render the hit bar
 		for i, sym := range barSyms {
-			str += fill(cis[i], int64(ws.Row)-bottomPadding, sym)
+			str += fill(cis[i], rc-bottomPadding, sym)
 		}
 
 		// Render notes
 		for _, line := range chart.notes {
-			mulstr, miss := line.step(now, currentDuration, cis, int64(ws.Row))
+			mulstr, miss := line.step(now, currentDuration, cis, rc)
 			str += mulstr
 			counts[len(counts)-1] += miss
 		}
@@ -477,12 +568,15 @@ func run() error {
 		remainingTime := deadline.Sub(time.Now())
 		renderTime := framePeriod.Nanoseconds() - remainingTime.Nanoseconds()
 
-		str += side(2, fmt.Sprintf("Render Time:  %5.0f µs", float64(renderTime)/1000.0))
-		str += side(3, fmt.Sprintf("  Idle Time:  %.1f%%", 100-100*float64(renderTime)/float64(framePeriod.Nanoseconds())))
-		str += side(10, fmt.Sprintf("   Error dt:  %6v", score))
-		str += side(11, fmt.Sprintf("      Total:  %6v", totalNotes))
+		str += side(2, sideCol, fmt.Sprintf("Render Time:  %5.0f µs", float64(renderTime)/1000.0))
+		str += side(3, sideCol, fmt.Sprintf("  Idle Time:  %.1f%%", 100-100*float64(renderTime)/float64(framePeriod.Nanoseconds())))
+		str += side(10, sideCol, fmt.Sprintf("   Error dt:  %6v", score))
+		str += side(11, sideCol, fmt.Sprintf("      Stdev:  %6.2f", stdev))
+		str += side(12, sideCol, fmt.Sprintf("       Mean:  %6.2f", mean))
+		str += side(13, sideCol, fmt.Sprintf("      Total:  %6v", chart.noteCount))
+		str += side(14, sideCol, fmt.Sprintf("      Mines:  %6v", chart.mineCount))
 		for i, count := range counts {
-			str += side(13+i, fmt.Sprintf("%v:  %6v", judgementNames[i], count))
+			str += side(18+i, sideCol, fmt.Sprintf("%v:  %6v", judgementNames[i], count))
 		}
 		fmt.Print(str)
 
